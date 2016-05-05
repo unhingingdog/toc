f04a86fbfb13fe37cab9afb6bb95444752215615
HAMISH <constatcopywriting@gmail.com>
2016-05-05 20:33:42 +1200

    make user model with password

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 group :development, :test do
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505023404) do
+ActiveRecord::Schema.define(version: 20160505080624) do
 
   create_table "disputes", force: :cascade do |t|
     t.string   "title"
     t.text     "situation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

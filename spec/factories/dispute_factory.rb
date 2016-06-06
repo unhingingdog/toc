ede30884628bd5b09a31ac04ccef5b7e0501ff3c
HAMISH <constatcopywriting@gmail.com>
2016-06-06 13:53:56 +1200

    fixed delete dispute feature spec

--- spec/factories/dispute_factory.rb
+++ spec/factories/dispute_factory.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :dispute do
     title "Example dispute"
     situation "Example Situation"
+    issue "Example issue"
+    crowd_size "Example crowd size"
+    activated true
   end
 end
--- spec/features/deleting_projects_spec.rb
+++ spec/features/deleting_projects_spec.rb
@@ -3,24 +3,21 @@ require 'spec_helper'
 feature "Deleting a dispute" do
   before do
     visit '/'
-    click_link 'Sign in'
     user = FactoryGirl.create(:user)
+    click_link 'Sign in'
     fill_in 'Name', with: user.name
     fill_in 'Password', with: user.password
     click_button 'Sign in'
   end
+
   scenario "Deleting a dispute" do
-    FactoryGirl.create(:dispute, title: "The fridge is fucked")
-
-    visit root_path
-
-    click_link "The fridge is fucked"
+    FactoryGirl.create(:dispute, title: "The fridge is broken", user_id: 1)
+    visit "/users/1"
+    click_link "The fridge is broken"
     click_link "Revoke Dispute"
-
     expect(page).to have_content("Dispute has been revoked.")
 
-    visit "/"
-
-    expect(page).to have_no_content("The fridge is fucked")
+    visit "/users/1"
+    expect(page).to have_no_content("The fridge is broken")
   end
 end
